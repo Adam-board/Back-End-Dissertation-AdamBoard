548b6f7fc0417680c5a366292385c929cb548a09
Adam-board <adamboard2003@gmail.com>
2024-04-29 14:52:21 -0700

    Broken venv, now fixed

--- Blueprints/exportJira.py
+++ Blueprints/exportJira.py
@@ -1,12 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from jira import JIRA
 from Blueprints.database import reports, vulns
 from bson import ObjectId
+
 
 exportJiraBlueprint = Blueprint('exportJiraBlueprint', __name__)
 
 
 #Grabs all Vulnerabilities for the report to export as Jira Tickets
-@exportJiraBlueprint.route("/api/report/<ReportID>/exportjira", methods=["GET"])
-def ExportJira(ReportID):
+@exportJiraBlueprint.route("/api/report/exportjira", methods=["POST"])
+def ExportJira():
+    data = request.json
+
+    ReportID = data["id"]
+
+
+    report = reports.find_one({"_id": ObjectId(ReportID)})
     return {"Heading": "Exporting to Jira as Tickets"}
--- Blueprints/exportWord.py
+++ Blueprints/exportWord.py
@@ -1,12 +1,24 @@
-from flask import Blueprint
-
+from flask import Blueprint, jsonify, request
+from docx import Document
 from Blueprints.database import reports, vulns, sections
 from bson import ObjectId
-
 
 exportWordBlueprint = Blueprint('exportWordBlueprint', __name__)
 
 #GETs the selected report for exporting to Word
-@exportWordBlueprint.route("/api/report/<ReportID>/exportword", methods=["GET"])
-def ExportWord(ReportID):
+@exportWordBlueprint.route("/api/report/exportword", methods=["POST"])
+def ExportWord():
+    data = request.json
+
+    ReportID = data["id"]
+
+    ExecutiveSummary = {{}}
+    Sections = {{}}
+    CriticalVulns = {{}}
+    HighVulns = {{}}
+    MediumVulns = {{}}
+    LowVulns = {{}}
+ 
+
+    report = reports.find_one({"_id": ObjectId(ReportID)})
     return {"Heading": "Exporting to Word"}
