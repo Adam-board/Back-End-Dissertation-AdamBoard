e0fc8948836da1df7252404a431caac43e643c4e
Adam-board <adamboard2003@gmail.com>
2024-04-29 22:45:12 -0700

    JIRA Almost works, however lack of knowledge led this to be the only feature that did not reach implementation

--- Blueprints/exportJira.py
+++ Blueprints/exportJira.py
@@ -4,10 +4,32 @@ from Blueprints.database import reports, vulns
 from bson import ObjectId
 from draftjs_exporter.html import HTML
 import json
-
+from draftjs_exporter.html import HTML
+from bs4 import BeautifulSoup
 
 exportJiraBlueprint = Blueprint('exportJiraBlueprint', __name__)
 
+
+
+# Jira API credentials
+JIRA_URL = 'https://report-writing-project.atlassian.net'
+JIRA_USERNAME = 'Adam Board'
+JIRA_API_TOKEN = 'ATATT3xFfGF0t46feRg1S73dJJz05woPnqHXxSN1Av3bh8uVBwRgXZtNAXuufHFLBMRoQnctHRaaPMIpQxGwyG-AddRuj94-cW95cWcZyj6fLAob1cZSNf4qvXc7NaWrYuKnqIGW7SGUagStOAH0dzJBBguUUzCvrYCBw2c4pfQw-Xlm1Q8XouY=6D7A0DFD'
+JIRA_PROJECT_KEY = 'REP'
+
+
+jira_client = JIRA(JIRA_URL, basic_auth=(JIRA_USERNAME, JIRA_API_TOKEN))
+
+# Function to create a Jira issue
+def create_jira_issue(summary, description, labels):
+    issue_dict = {
+        'project': {'key': JIRA_PROJECT_KEY},
+        'summary': summary,
+        'description': description,
+        'issuetype': {'name': 'Bug'},  # Change the issue type if needed
+    }
+
+    return jira_client.create_issue(fields=issue_dict)
 
 #Grabs all Vulnerabilities for the report to export as Jira Tickets
 @exportJiraBlueprint.route("/api/report/exportjira", methods=["POST"])
@@ -38,26 +60,33 @@ def ExportJira():
             if vuln:
                 vulnsData.append(vuln)
 
-        # Convert each vulnerability to HTML
-        htmlOutputsVulns = []
         for vulnerability in vulnsData:
             if vulnerability['Data']:
                 dataVuln = json.loads(vulnerability['Data'])
                 htmlOutputVuln = html_exporter.render(dataVuln)
-                htmlOutputsVulns.append(htmlOutputVuln)
-                vulnerability['Data'] = htmlOutputVuln
-
-        for vulnerability in vulnsData:
-            if vulnerability['Severity'] == 'Critical':
-                CriticalVulns.append(vulnerability)
-            elif vulnerability['Severity'] == 'High':
-                HighVulns.append(vulnerability)
-            elif vulnerability['Severity'] == 'Medium':
-                MediumVulns.append(vulnerability)
-            elif vulnerability['Severity'] == 'Low':
-                LowVulns.append(vulnerability)
-
-    
+                vulnerability['Data'] = convert_html_to_rich_text(htmlOutputVuln)
+                create_jira_issue(vulnerability['VulnName'], vulnerability['Data'], vulnerability['Severity'])
 
 
     return {"Heading": "Exporting to Jira as Tickets"}
+
+
+def convert_html_to_rich_text(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    formatted_text = ''
+    for tag in soup.find_all():
+        if tag.name == 'h1':
+            formatted_text += f'\n\n{tag.text.upper()}\n'  # Convert to uppercase for heading 1
+        elif tag.name in ['h2', 'h3', 'h4', 'h5', 'h6']:
+            formatted_text += f'\n\n{tag.text}\n'
+        elif tag.name == 'p':
+            formatted_text += f'\n{tag.text}\n'
+        elif tag.name == 'code':
+            formatted_text += f'\n{tag.text}\n'  # Assuming code tag represents code block
+        elif tag.name == 'ul':
+            for li in tag.find_all('li'):
+                formatted_text += f'\n- {li.text}\n'  # Convert list items to bullet points
+        elif tag.name == 'ol':
+            for index, li in enumerate(tag.find_all('li'), start=1):
+                formatted_text += f'\n{index}. {li.text}\n'  # Convert list items to numbered list
+    return formatted_text.strip()
